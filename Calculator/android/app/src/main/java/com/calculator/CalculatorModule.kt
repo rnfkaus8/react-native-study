d5f1d6d1e0bf7dc2fe455f9c1289f908254bec38
rnfkaus8 <49504326+rnfkaus8@users.noreply.github.com>
2023-02-19 17:37:24 +0900

    Delete Calculator directory

--- Calculator/android/app/src/main/java/com/calculator/CalculatorModule.kt
+++
@@ -1,8 +0,0 @@
-package com.calculator
-
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-
-class CalculatorModule(reactContext: ReactApplicationContext): ReactContextBaseJavaModule(reactContext) {
-    override fun getName() = "CalculatorModule"
-}
